8aea4511155396e4ebd3b53b0e6a11d473be50e4
Gyusun Yeom <omniavinco@gmail.com>
2016-12-11 16:11:43 +0900

    add id for node

--- assignment3/Node.hpp
+++ assignment3/Node.hpp
@@ -7,12 +7,15 @@ class Node {
 public:
   explicit Node(SHPObject *obj)
     : _point(obj->dfXMin, obj->dfYMax)
+    , _id(obj->nShapeId)
   {}
 
+  int id() const { return _id; }
   double x() const { return _point.x; }
   double y() const { return _point.y; }
 
   const PointD &point() const { return _point; }
 private:
   PointD _point;
+  int _id;
 };
